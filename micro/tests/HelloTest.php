34b45d95acbf95224128c85cd844e8da0bf9d952
Zcasper <testuser@mail.linpax.org>
2015-01-15 15:43:40 +0600

    Hello test

---
+++ micro/tests/HelloTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Micro\tests;
+
+
+class HelloTest extends \PHPUnit_Framework_TestCase
+{
+	public function up()
+	{
+		//
+	}
+
+	public function down()
+	{
+		//
+	}
+
+	public function testHello()
+	{
+		$this->assertEquals('200 Ok', '200 Ok');
+	}
+}
