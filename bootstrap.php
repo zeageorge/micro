d48d917db1691df7d6e9e70325be62f18c5265d6
Zcasper <testuser@mail.linpax.org>
2015-01-15 15:29:57 +0600

    Patch grid

--- bootstrap.php
+++ bootstrap.php
@@ -4,11 +4,11 @@
 //define('DEBUG_MICRO', true);
 
 // Configs
-$config = require __DIR__ . '/../app/configs/index.php';
+$config = require __DIR__ . '/app/configs/index.php';
 
 // Get micro
 require $config['MicroDir'] . '/base/Autoload.php';
 require $config['MicroDir'] . '/Micro.php';
 
 // Run application
-\Micro\Micro::getInstance($config)->run();
+\Micro\Micro::getInstance($config);
